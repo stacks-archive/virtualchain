f7fe506f01fd7056d6c9e063c39c20eed746a484
Jude Nelson <judecn@gmail.com>
2015-08-11 14:22:38 -0400

    Switch to GPLv3 and update copyright statement

--- virtualchain/lib/blockchain/__init__.py
+++ virtualchain/lib/blockchain/__init__.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    virtualchain
+    Virtualchain
     ~~~~~
-    :copyright: (c) 2015 by Openname.org
-    :license: MIT, see LICENSE for more details.
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+    
+    This file is part of Virtualchain
+    
+    Virtualchain is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Virtualchain is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Virtualchain.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import transactions 
 import session
 
 from transactions import get_bitcoind, getrawtransaction, getrawtransaction_async, getblockhash, getblockhash_async, getblock, getblock_async, get_sender_and_amount_in_from_txn, \
-   get_senders_and_total_in, get_total_out, process_nulldata_tx, process_nulldata_tx_async, get_nulldata_txs_in_blocks, get_nulldata_txs_in_block
+   get_total_out, process_nulldata_tx_async, get_nulldata_txs_in_blocks
 from nulldata import get_nulldata, has_nulldata
 from session import BitcoindConnection, create_bitcoind_connection, connect_bitcoind
