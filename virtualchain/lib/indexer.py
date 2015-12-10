e3eee764fd98366ab8a11575c7a23ac89508f820
Muneeb Ali <muneeb@ali.vc>
2015-12-10 14:42:19 -0500

    license year updates

--- virtualchain/lib/indexer.py
+++ virtualchain/lib/indexer.py
@@ -3,8 +3,8 @@
 """
     Virtualchain
     ~~~~~
-    copyright: (c) 2014 by Halfmoon Labs, Inc.
-    copyright: (c) 2015 by Blockstack.org
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
     
     This file is part of Virtualchain
     
