4ee267c09b875ab60d70a5d93db1aa497815c699
Jude Nelson <judecn@gmail.com>
2017-10-25 15:23:26 -0400

    use cryptography.io instead of python-ecdsa for digest-based signing and verifying (20x speed improvement)

--- virtualchain/lib/ecdsalib.py
+++ virtualchain/lib/ecdsalib.py
@@ -21,16 +21,11 @@
      along with Virtualchain.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from ecdsa import SigningKey, VerifyingKey, SECP256k1
-from ecdsa.util import sigencode_der, sigdecode_der
-from ecdsa import BadSignatureError
-
 import keylib
-from keylib import ECPrivateKey 
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import ec, utils
 from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature, encode_dss_signature
 from cryptography.exceptions import InvalidSignature
 
@@ -38,8 +33,12 @@ from .config import get_logger
 
 import base64
 import hashlib
+import re
+from binascii import hexlify
 
 log = get_logger("virtualchain-ecdsalib")
+
+SECP256k1_order = 115792089237316195423570985008687907852837564279074904382605163141518161494337L
 
 class ECSigner(object):
     """
@@ -109,22 +108,122 @@ class ECVerifier(object):
             return False
 
 
-def ecdsa_private_key(privkey_str=None):
+class _ECPrivateKey(object):
+    _pubkeyhash_version_byte = 0
+
+    def __init__(self, private_key=None, compressed=True):
+        """ Takes in a private key/secret exponent.
+        """
+        pk_i = None
+        if private_key is None:
+            pk_i = ec.generate_private_key(ec.SECP256K1(), default_backend()).private_numbers().private_value
+        else:
+            pk_i = keylib.key_formatting.encode_privkey(private_key, 'decimal')
+
+        privkey_str = '{:064x}'.format(pk_i)
+        assert len(privkey_str) == 64
+
+        self._ecdsa_private_key_string = privkey_str.decode('hex')
+        self._compressed = compressed
+
+    @classmethod
+    def wif_version_byte(cls):
+        if hasattr(cls, '_wif_version_byte'):
+            return cls._wif_version_byte
+        return (cls._pubkeyhash_version_byte + 128) % 256
+
+    def to_bin(self):
+        if self._compressed:
+            return keylib.key_formatting.encode_privkey(
+                self._ecdsa_private_key_string, 'bin_compressed')
+        else:
+            return self._ecdsa_private_key_string
+
+    def to_hex(self):
+        if self._compressed:
+            return keylib.key_formatting.encode_privkey(
+                self._ecdsa_private_key_string, 'hex_compressed')
+        else:
+            return hexlify(self.to_bin())
+
+    def to_wif(self):
+        if self._compressed:
+            return keylib.key_formatting.encode_privkey(
+                self._ecdsa_private_key_string, 'wif_compressed', vbyte=self._pubkeyhash_version_byte)
+        else:
+            return keylib.b58check.b58check_encode(
+                self.to_bin(), version_byte=self.wif_version_byte())
+
+    def public_key(self):
+        # lazily calculate and set the public key
+        if not hasattr(self, '_public_key'):
+
+            privk = ec.derive_private_key(int(self._ecdsa_private_key_string.encode('hex'), 16), ec.SECP256K1(), default_backend())
+            pubk = privk.public_key()
+
+            ecdsa_public_key_str = pubk.public_numbers().encode_point().encode('hex')
+            if self._compressed:
+                ecdsa_public_key_str = keylib.key_formatting.compress(ecdsa_public_key_str)
+
+            self._public_key = _ECPublicKey(ecdsa_public_key_str, version_byte=self._pubkeyhash_version_byte)
+
+        # return the public key object
+        return self._public_key
+
+
+class _ECPublicKey(object):
+    _version_byte = 0
+
+    @classmethod
+    def version_byte(cls):
+        return cls._version_byte
+
+    def __init__(self, public_key_string, version_byte=None, verify=True):
+        """ Takes in a public key in hex format.
+        """
+        # set the version byte
+        if version_byte:
+            self._version_byte = version_byte
+
+        self._charencoding, self._type = keylib.public_key_encoding.get_public_key_format(public_key_string)
+
+        # extract the binary key (compressed/uncompressed w magic byte)
+        self._bin_public_key = keylib.public_key_encoding.extract_bin_ecdsa_pubkey(public_key_string)
+
+        if verify:
+            pubkey_hex_decompressed = keylib.key_formatting.decompress(self._bin_public_key.encode('hex'))
+            pubk = ec.EllipticCurvePublicNumbers.from_encoded_point(ec.SECP256K1(), pubkey_hex_decompressed.decode('hex')).public_key(default_backend())
+
+    def to_bin(self):
+        return self._bin_public_key
+
+    def to_hex(self):
+        return hexlify(self.to_bin())
+
+    def bin_hash160(self):
+        if not hasattr(self, '_bin_hash160'):
+            binary_key = self.to_bin()  
+            if self._type == keylib.public_key_encoding.PubkeyType.compressed:
+                binary_key = keylib.key_formatting.compress(binary_key)
+
+            self._bin_hash160 = keylib.hashing.bin_hash160(binary_key)
+        return self._bin_hash160
+
+    def hash160(self):
+        return hexlify(self.bin_hash160())
+
+    def address(self):
+        return keylib.address_formatting.bin_hash160_to_address(
+            self.bin_hash160(), version_byte=self._version_byte)
+
+
+
+def ecdsa_private_key(privkey_str=None, compressed=True):
     """
     Make a private key, but enforce the following rule:
     * unless the key's hex encoding specifically ends in '01', treat it as uncompressed.
     """
-    compressed = False
-    if privkey_str is not None:
-        if not isinstance(privkey_str, (str, unicode)):
-            raise ValueError("Private key is not a string")
-
-        privkey_str = str(privkey_str)
-
-    if privkey_str is None or keylib.key_formatting.get_privkey_format(privkey_str).endswith('compressed'):
-        compressed = True
-
-    return ECPrivateKey(privkey_str, compressed=compressed)
+    return _ECPrivateKey(privkey_str, compressed=compressed)
 
 
 def set_privkey_compressed(privkey, compressed=True):
@@ -220,10 +319,7 @@ def decode_pubkey_hex(pubkey_hex):
     if not isinstance(pubkey_hex, (str, unicode)):
         raise ValueError("public key is not a string")
 
-    pubk = str(pubkey_hex)
-    if keylib.key_formatting.get_pubkey_format(pubk) == 'hex_compressed':
-        pubk = keylib.key_formatting.decompress(pubk)
-
+    pubk = keylib.key_formatting.decompress(str(pubkey_hex))
     assert len(pubk) == 130
 
     pubk_raw = pubk[2:]
@@ -236,9 +332,9 @@ def encode_signature(sig_r, sig_s):
     Encode an ECDSA signature, with low-s
     """
     # enforce low-s 
-    if sig_s * 2 >= SECP256k1.order:
+    if sig_s * 2 >= SECP256k1_order:
         log.debug("High-S to low-S")
-        sig_s = SECP256k1.order - sig_s
+        sig_s = SECP256k1_order - sig_s
 
     sig_bin = '{:064x}{:064x}'.format(sig_r, sig_s).decode('hex')
     assert len(sig_bin) == 64
@@ -303,12 +399,12 @@ def sign_digest(hash_hex, privkey_hex, hashfunc=hashlib.sha256):
 
     hash_hex = str(hash_hex)
 
-    pk_uncompressed_hex, pubk_uncompressed_hex = get_uncompressed_private_and_public_keys(privkey_hex)
+    pk_i = decode_privkey_hex(privkey_hex)
+    privk = ec.derive_private_key(pk_i, ec.SECP256K1(), default_backend())
 
-    sk = SigningKey.from_string(pk_uncompressed_hex.decode('hex'), curve=SECP256k1)
-    sig_bin = sk.sign_digest_deterministic(hash_hex.decode('hex'), sigencode=sigencode_der, hashfunc=hashfunc)
-    
-    sig_r, sig_s = sigdecode_der( sig_bin, SECP256k1.order )
+    sig = privk.sign(hash_hex.decode('hex'), ec.ECDSA(utils.Prehashed(hashes.SHA256())))
+
+    sig_r, sig_s = decode_dss_signature(sig)
     sigb64 = encode_signature(sig_r, sig_s)
     return sigb64
 
@@ -327,16 +423,15 @@ def verify_digest(hash_hex, pubkey_hex, sigb64, hashfunc=hashlib.sha256):
         raise ValueError("hash hex is not a string")
 
     hash_hex = str(hash_hex)
-
-    sig_r, sig_s = decode_signature(sigb64)
     pubk_uncompressed_hex = keylib.key_formatting.decompress(pubkey_hex)
+    sig_r, sig_s = decode_signature(sigb64)
 
-    sig_bin = sigencode_der( sig_r, sig_s, SECP256k1.order )
-    vk = VerifyingKey.from_string(pubk_uncompressed_hex[2:].decode('hex'), curve=SECP256k1)
+    pubk = ec.EllipticCurvePublicNumbers.from_encoded_point(ec.SECP256K1(), pubk_uncompressed_hex.decode('hex')).public_key(default_backend())
+    signature = encode_dss_signature(sig_r, sig_s)
 
     try:
-        res = vk.verify_digest(sig_bin, hash_hex.decode('hex'), sigdecode=sigdecode_der)
-        return res
-    except BadSignatureError:
-        log.debug("Bad signature {}; not from {} on {}?".format(sigb64, pubkey_hex, hash_hex))
+        pubk.verify(signature, hash_hex.decode('hex'), ec.ECDSA(utils.Prehashed(hashes.SHA256())))
+        return True
+    except InvalidSignature:
         return False
+
