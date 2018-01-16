16a457dfe0217601c74a97ab219c50285d7767c1
Jude Nelson <judecn@gmail.com>
2018-01-16 16:38:58 -0500

    make address_reencode() a blockchain-agnostic method, separate from bitcoin code

---
+++ virtualchain/lib/blockchain/address.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Virtualchain
+    ~~~~~
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015-2018 by Blockstack.org
+    
+    This file is part of Virtualchain
+    
+    Virtualchain is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Virtualchain is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Virtualchain.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from bitcoin_blockchain import btc_address_reencode
+
+def address_reencode(address, blockchain='bitcoin', **blockchain_opts):
+    """
+    Reencode an address
+    """
+    if blockchain == 'bitcoin':
+        return btc_address_reencode(address, **blockchain_opts)
+    else:
+        raise ValueError("Unknown blockchain '{}'".format(blockchain))
